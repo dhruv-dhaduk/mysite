d71357495ab8cdb95d71f1015c145268d48c3e72
Dhruv Dhaduk <dhadukd44@gmail.com>
2023-06-04 21:36:05 +0530

    Added headings page

--- dhruv/views.py
+++ dhruv/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "dhruv/index.html")
+
+def headings(request):
+    return render(request, "dhruv/headings.html")
